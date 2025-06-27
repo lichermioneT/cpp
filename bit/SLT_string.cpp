8726802f23b0c3080d89692d3b8aac31985aa01d
lic <2924942127@qq.com>
2025-06-27 10:37:56 +0800

    模版

---
+++ bit/SLT_string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// 函数实现交换两个变量
+template<class T>
+void swap(T& a, T& b)
+{
+
+    
+
+}
+
+
+
+int main()
+{
+
+
+
+
+
+
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
