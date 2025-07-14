11852168bf3a2edecb4c7d9d996adf210440bc66
lic <2924942127@qq.com>
2025-07-14 16:42:21 +0800

    c++类

--- 6c++类.cpp
+++ 6c++类.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class date
 {
 public:
-    date(int year = 0, int month = 1; int day = 1)
+    date(int year = 0, int month = 1, int day = 1)
     {
         _year = year;
         _month = month;
@@ -53,14 +53,48 @@ public:
     {
         return this;
     }
+private:
+    int _year;
+    int _month;
+    int _day;
+};
 
 
+class date1
+{
+public:
+    date1(int year, int month, int day)
+        :_year(year)
+        ,_month(month)
+        ,_day(day)
+        ,_ref(_day)
+    {
+        // 初始化列表
+        // 为什么有初始化列表呢？
+        // 引用成员，const成员，没有默认构造函数的类(不用传参数的构造函数)
+    }
+private:
+    int _year;
+    int _month;
+    int _day;
+    int& _ref;
+};
 
+class a
+{
+public:
+    a(int a)
+        :_a(a)
+    {
 
+    }
 
+private:
+    int _a;
+};
 
-
-
+class b
+{
 
 private:
     int _year;
@@ -69,11 +103,93 @@ private:
 };
 
 
+// static成员
+// 静态成员变量
+// 计算一个类产生了多少个对象
+// 对象一定是构造函数，拷贝构造产生的
 
+
+class F
+{
+public:
+    F()
+    {
+        ++n;
+    }
+    
+    F(const F& f)
+    {
+        ++n;
+    }
+
+    static int getn() // 没有this指针 不能访问非静态成员的
+    {
+        return n;
+    }
+
+private:
+    static int n;
+};
+
+
+// 静态成员属性，不属于任何一个类
+// 必须在外面初始化
+
+int F::n = 0;
+
+F f(F f)
+{
+    return f;    
+}
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+
+    // b b1(1,2); // 对象定义
+
+
+    // d d1(2);
+    // d d2 = 9; // 创建临时变量，用来赋值d2 临时对象9给d2
+    // const d& d2 = 9; //  中间就不会优化了,就是临时对象的
+    // d d3 = d1; // 拷贝构造实现
+
+    // int i = 0;
+    // const double& d  = i;
+    
+    // c++11支持
+    // E e = {1,2,3};
+
+    F f1;
+    F f2;
+
+    f(f1);
+    f(f2);
+    
+   cout<< (F::getn()) <<endl; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
 
